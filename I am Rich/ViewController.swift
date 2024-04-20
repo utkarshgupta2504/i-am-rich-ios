614869dc1ec36170b31b36507840eb14fdebe350
Utkarsh Gupta <52123629+utkarshgupta2504@users.noreply.github.com>
2024-04-20 16:41:22 +0530

    Initial Commit

---
+++ I am Rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I am Rich
+//
+//  Created by Utkarsh Gupta on 20/04/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
